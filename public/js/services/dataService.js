42b956c43af9bb0696cd79c7e93a03f01634df14
Stephen Done <srdone@gmail.com>
2015-02-23 22:53:37 -0700

    summarized data

--- public/js/services/dataService.js
+++ public/js/services/dataService.js
@@ -23,17 +23,49 @@ angular.module('githubAPI').factory('dataService',
         }));
       };
 
+      var _getCommits = function (name, url) {
+        promises.push($http.get(url + auth).then(function (response) {
+          var commitsResponse = response.data;
+          var personalCommitCount = 0;
+          for (var i = 0; i < commitsResponse; i++) {
+            if (commitsResponse[i].commit.author === username) {
+              personalCommitCount++;
+            }
+          };
+          data.repos[name].personalCommitCount = personalCommitCount;
+          data.repos[name].commits = commitsResponse;
+        }));
+      };
+
       promises.push($http.get('http://api.github.com/users/' + username + '/repos' + auth).then(function (response) {
         data.repos = {};
         data.repos.summary = response.data;
-        debugger;
         for (var i = 0; i < response.data.length; i++) {
-          debugger;
           _getLanguages(response.data[i].name, response.data[i], response.data[i].languages_url);
+          //_getCommits(response.data[i].name, response.data[i].commits_url);
         }
       }));
 
-      return $q.all(promises).then(function () {
+      var _summarizeData = function () {
+        var summarizedRepoData = [];
+        for (var i = 0; i < data.repos.summary.length; i++) {
+          summarizedRepoData.push({
+            name: data.repos.summary[i].full_name,
+            url: data.repos.summary[i],
+            fork: data.repos.summary[i].fork,
+            forkCount: data.repos.summary[i].forks_count,
+            age: Date.now() - new Date(data.repos.summary[i].created_at), //milliseconds
+            size: data.repos.summary[i].size,
+            watchers: data.repos.summary[i].watchers_count,
+            staleness: Date.now() - new Date(data.repos.summary[i].updated_at) //milliseconds
+            //languages: _summarizeLanguages(data.repos[key])
+          });
+        }
+        data.summarizedRepoData = summarizedRepoData;
+      };
+
+      return $q.all(promises).then(function (result) {
+        _summarizeData();
         return data;
       });
     };
